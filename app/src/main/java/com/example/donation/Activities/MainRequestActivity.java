cd69f83e658d1ea61b04c1fdee462f094a3ec7ec
devansh071999 <devanshsampatjain@gmail.com>
2020-09-06 13:57:17 +0530

    uploading function created

--- app/src/main/java/com/example/donation/Activities/MainRequestActivity.java
+++ app/src/main/java/com/example/donation/Activities/MainRequestActivity.java
@@ -1,8 +1,10 @@
 package com.example.donation.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.donation.R;
+
+import java.net.URI;
 
 public class MainRequestActivity extends AppCompatActivity {
 
@@ -19,6 +24,7 @@ public class MainRequestActivity extends AppCompatActivity {
     TextView chooseImageText;
     ImageView postImage;
     Button submit_button;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +37,7 @@ public class MainRequestActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (isValid()){
-
+                    uploadRequest(messageText.getText().toString());
                 }
             }
         });
@@ -44,7 +50,25 @@ public class MainRequestActivity extends AppCompatActivity {
                 startActivityForResult(intent,101);
             }
         });
+    }
 
+    private void uploadRequest(String message){
+
+    }
+
+    private void uploadImage(){
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==101 && requestCode==RESULT_OK){
+            if(data!=null){
+                imageUri=data.getData();
+                Glide.with(getApplicationContext()).load(imageUri).into(postImage);
+            }
+        }
     }
 
     private boolean isValid(){
